207492433bc0842ae078465afc7aa682d6326278
oussama <osm2014@hotmail.fr>
2023-04-03 21:58:19 +0000

    fix links cursor

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <Link to="home">
               <div className="container flex items-center space-x-2">
-                <h2 className="text-2xl font-bold">Oussama Reghay</h2>
+                <h2 className="text-2xl font-bold cursor-pointer">Oussama Reghay</h2>
               </div>
             </Link>
             <div className="md:hidden">
@@ -66,7 +66,7 @@ export default function Navbar() {
                     key={idx}
                     to={item.page}
                     className={
-                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                      "block lg:inline-block text-neutral-900 cursor-pointer  hover:text-neutral-500 dark:text-neutral-100"
                     }
                     activeClass="active"
                     spy={true}
